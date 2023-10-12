90c0cb0e795e20e62edf8ce0d24910cad6bfc051
iyes826 <treeyu12345@naver.com>
2023-10-12 12:40:41 +0900

    ✨ :: 앱 색상 상수 지정

---
+++ lib/core/constant/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color realGreen = const Color(0xFF2BA52E);
+  static Color green = const Color(0xFF67C971);
+
+  static Color orange = const Color(0xFFEF8436);
+
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+
+  static Color primaryColor = const Color(0xFF626262);
+}
