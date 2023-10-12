b7edcea13f20c713b2c51e83485bc3092aaf6d89
iyes826 <treeyu12345@naver.com>
2023-10-12 21:20:46 +0900

    ✨ :: 스플래쉬 스크린 추가

--- lib/core/view/splash_screen.dart
+++ lib/core/view/splash_screen.dart
@@ -1,10 +1,22 @@
+import 'package:easy_flutter/core/constant/app_color.dart';
+import 'package:easy_flutter/presentation/widget/text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Scaffold(
+      body: Column(
+        children: [
+          const Spacer(),
+          Image.asset(
+            'assets/images/image.png',
+          ),
+        ],
+      ),
+    );
   }
 }
