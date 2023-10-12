9005df4d3c0d7723eb2592dd87eb9569a6fd2c63
iyes826 <treeyu12345@naver.com>
2023-10-12 12:41:33 +0900

    ✨ :: flutter_screenutil 라이브러리 기본 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
+import 'package:easy_flutter/core/view/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ScreenUtilInit(
+      designSize: const Size(375, 824),
+      builder: (context, child) => MaterialApp(
+        theme: ThemeData(
+          fontFamily: 'Pretendard',
+        ),
+        debugShowCheckedModeBanner: false,
+        home: child,
+      ),
+      child: const SplashScreen(),
     );
   }
 }
